a43421b7f519adf4263ab094fdd540fb2c36e9a0
Leonardo Braz <lhleonardo@hotmail.com>
2020-03-26 01:57:52 -0300

    Create and show repository's info

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Repository() {
-  return <h1>Olá, você está na pagina Repository!</h1>;
+import { Link } from 'react-router-dom';
+
+import api from '../../service/api';
+import { Loading, Owner, IssueList, Issue, Label, LabelList } from './styles';
+
+import Container from '../../components/Container';
+
+function Repository({ match: { params } }) {
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const repoName = decodeURIComponent(params.repoName);
+
+  useEffect(() => {
+    async function loadInfo() {
+      setLoading(true);
+      // base_api/repoName/issues
+      const [repoInfo, repoIssues] = await Promise.all([
+        await api.get(`/repos/${repoName}`),
+        await api.get(`/repos/${repoName}/issues`, {
+          params: { state: 'open', per_page: 10 },
+        }),
+      ]);
+
+      setRepository(repoInfo.data);
+      setIssues(repoIssues.data);
+      setLoading(false);
+    }
+
+    loadInfo();
+  }, [repoName]);
+  return loading ? (
+    <Loading>Carregando...</Loading>
+  ) : (
+    <Container>
+      <Owner>
+        <Link to="/">Voltar para os repositórios</Link>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+
+      <IssueList>
+        {issues.map((issue) => (
+          <Issue key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+              </strong>
+              <p>{issue.user.login}</p>
+
+              <LabelList>
+                {issue.labels.map((label) => (
+                  <Label key={String(label.id)} color={label.color}>
+                    <span>{label.name}</span>
+                  </Label>
+                ))}
+              </LabelList>
+            </div>
+          </Issue>
+        ))}
+      </IssueList>
+    </Container>
+  );
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repoName: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Repository;
---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,124 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+`;
+
+export const Owner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    text-decoration: none;
+    color: #5960c1;
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  img {
+    width: 120px;
+    border-radius: 50%;
+    margin-top: 20px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  p {
+    margin-top: 5px;
+    font-size: 14px;
+
+    color: #666;
+
+    line-height: 1.4;
+    text-align: center;
+
+    max-width: 400px;
+  }
+`;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+
+  list-style: none;
+`;
+
+export const Issue = styled.li`
+  display: flex;
+  padding: 15px 10px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+
+  & + li {
+    margin-top: 10px;
+  }
+  img {
+    width: 80px;
+
+    border-radius: 50%;
+  }
+
+  div {
+    flex: 1;
+    margin-left: 15px;
+
+    strong {
+      font-size: 16px;
+
+      a {
+        text-decoration: none;
+        color: #333;
+
+        &:hover {
+          color: #5960c1;
+        }
+      }
+    }
+
+    p {
+      margin: 5px 0px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
+`;
+
+function invertHex(hex) {
+  return (Number(`0x1${hex}`) ^ 0xffffff).toString(16).substr(1).toUpperCase();
+}
+export const LabelList = styled.ul`
+  list-style: none;
+`;
+
+export const Label = styled.li`
+  display: inline;
+
+  background: #${(props) => props.color};
+  padding: 5px;
+  margin: 0;
+
+  line-height: bold;
+  mix-blend-mode: difference;
+
+  span {
+    color: inherit;
+    line-height: bold;
+  }
+
+  & + li {
+    margin-left: 10px;
+  }
+`;
